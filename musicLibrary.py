4f0c1eb247b411480305c1e6714594c3c834e3ae
Neha Sharma <s24_sharma_neha@mgmcen.ac.in>
2026-06-12 21:15:23 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music={
+    "vaari jawan":"https://www.youtube.com/watch?v=GG1_DsScm6U&list=RDGG1_DsScm6U&start_radio=1",
+    "dooron" : "https://www.youtube.com/watch?v=9T-Zbxg9X_4&list=RD9T-Zbxg9X_4&start_radio=1",
+    "ganga":"https://www.youtube.com/watch?v=ocRzt5NvI7A&list=RDocRzt5NvI7A&start_radio=1",
+    "bairan":"https://www.youtube.com/watch?v=oafxkMv4xnc&list=RDoafxkMv4xnc&start_radio=1"
+
+}
